4343b8e2845d7a154038301ef1477e231472c0bc
david <davidgilrosa1@gmail.com>
2026-04-28 23:47:50 +0200

    feat(dao): crear esqueleto de TipoPlazaDAO

---
+++ src/main/java/com/sqlpractica/dao/TipoPlazaDAO.java
@@ -0,0 +1,16 @@
+package com.sqlpractica.dao;
+
+import com.sqlpractica.db.Database;
+import com.sqlpractica.exception.DAOException;
+import com.sqlpractica.model.TipoPlaza;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TipoPlazaDAO {
+
+}
