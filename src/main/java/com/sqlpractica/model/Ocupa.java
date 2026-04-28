953a33cbbe4a59a9cd7931772ef5630780202d19
david <davidgilrosa1@gmail.com>
2026-04-28 23:43:57 +0200

    feat(model): añadir getters y setters en Ocupa

--- src/main/java/com/sqlpractica/model/Ocupa.java
+++ src/main/java/com/sqlpractica/model/Ocupa.java
@@ -20,4 +20,16 @@ public class Ocupa {
         this.fechaInicio = fechaInicio;
         this.fechaFin = fechaFin;
     }
+
+    public String getNssEmpleado() { return nssEmpleado; }
+    public void setNssEmpleado(String nssEmpleado) { this.nssEmpleado = nssEmpleado; }
+
+    public String getCodigoPlaza() { return codigoPlaza; }
+    public void setCodigoPlaza(String codigoPlaza) { this.codigoPlaza = codigoPlaza; }
+
+    public String getFechaInicio() { return fechaInicio; }
+    public void setFechaInicio(String fechaInicio) { this.fechaInicio = fechaInicio; }
+
+    public String getFechaFin() { return fechaFin; }
+    public void setFechaFin(String fechaFin) { this.fechaFin = fechaFin; }
 }
