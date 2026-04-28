60e6cc0c016cc69c52e647a7eb8457172799bd41
david <davidgilrosa1@gmail.com>
2026-04-28 23:41:52 +0200

    feat(model): crear POJO TipoPlaza con campos nombre y función

---
+++ src/main/java/com/sqlpractica/model/TipoPlaza.java
@@ -0,0 +1,13 @@
+package com.sqlpractica.model;
+
+/**
+ * Tipo de plaza (categoría laboral): nombre (PK) y función.
+ */
+public class TipoPlaza {
+
+    private String nombre;
+    private String funcion;
+
+    public TipoPlaza() {
+    }
+}
