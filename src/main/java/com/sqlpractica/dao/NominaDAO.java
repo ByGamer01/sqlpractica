d0a1ad423c48d938d5b7bdfda516e392b7ffe65b
david <davidgilrosa1@gmail.com>
2026-04-28 23:51:04 +0200

    feat(dao): crear esqueleto de NominaDAO

---
+++ src/main/java/com/sqlpractica/dao/NominaDAO.java
@@ -0,0 +1,17 @@
+package com.sqlpractica.dao;
+
+import com.sqlpractica.db.Database;
+import com.sqlpractica.exception.DAOException;
+import com.sqlpractica.model.Nomina;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NominaDAO {
+
+}
