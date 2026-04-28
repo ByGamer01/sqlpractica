bd72a705ad1ec8fb4f3139fc63b7d196f350b31f
david <davidgilrosa1@gmail.com>
2026-04-28 23:41:16 +0200

    feat(model): añadir getters y setters a Empleado

--- src/main/java/com/sqlpractica/model/Empleado.java
+++ src/main/java/com/sqlpractica/model/Empleado.java
@@ -22,4 +22,19 @@ public class Empleado {
         this.email = email;
         this.iban = iban;
     }
+
+    public String getNss() { return nss; }
+    public void setNss(String nss) { this.nss = nss; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getApellidos() { return apellidos; }
+    public void setApellidos(String apellidos) { this.apellidos = apellidos; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getIban() { return iban; }
+    public void setIban(String iban) { this.iban = iban; }
 }
