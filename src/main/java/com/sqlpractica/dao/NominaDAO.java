0007b5312df1b5c8d73f9b72e46ca074a54a87bf
david <davidgilrosa1@gmail.com>
2026-04-28 23:51:21 +0200

    feat(dao): implementar insert en NominaDAO recuperando ID autogenerado

--- src/main/java/com/sqlpractica/dao/NominaDAO.java
+++ src/main/java/com/sqlpractica/dao/NominaDAO.java
@@ -14,4 +14,23 @@ import java.util.List;
 
 public class NominaDAO {
 
+    public void insert(Nomina n) throws DAOException {
+        String sql = "INSERT INTO nomina(iban_pago, importe_pago, nss_empleado, codigo_plaza) " +
+                     "VALUES (?, ?, ?, ?)";
+        Connection conn = Database.getConnection();
+        try (PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            ps.setString(1, n.getIbanPago());
+            ps.setDouble(2, n.getImportePago());
+            ps.setString(3, n.getNssEmpleado());
+            ps.setString(4, n.getCodigoPlaza());
+            ps.executeUpdate();
+            try (ResultSet keys = ps.getGeneratedKeys()) {
+                if (keys.next()) {
+                    n.setId(keys.getInt(1));
+                }
+            }
+        } catch (SQLException e) {
+            throw new DAOException("No se ha podido crear la nómina: " + e.getMessage(), e);
+        }
+    }
 }
